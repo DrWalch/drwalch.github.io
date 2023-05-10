9f05f69c97d8bdc821f285317628ef5bde3f9784
DrWalch <131595643+DrWalch@users.noreply.github.com>
2023-05-10 19:08:55 +0200

    Update decode64.js

--- scripts/decode64.js
+++ scripts/decode64.js
@@ -2,3 +2,8 @@ var encodedStr = document.getElementById('coded').innerHTML;
 var decodedStr = atob(encodedStr);
 
 document.getElementById('coded').innerHTML = decodedStr;
+
+var encodedStr = document.getElementById('codedem').innerHTML;
+var decodedStr = atob(encodedStr);
+
+document.getElementById('codedem').innerHTML = decodedStr;
